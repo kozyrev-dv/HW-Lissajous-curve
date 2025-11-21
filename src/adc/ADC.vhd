e93be23b1ce79edaef39af41802c076fd2f7c37b
Dilimitria <dmalyga31@gmail.com>
2025-11-21 14:33:42 +0300

    изменение архитектуры считывания со второго канала

--- src/adc/ADC.vhd
+++ src/adc/ADC.vhd
@@ -30,11 +30,6 @@ architecture arch of ADC is
    signal clk_out                : std_logic                         := '0';
    signal clk_adc                : std_logic                         := '0';
 
-   --signal counter_adc            : integer                           := 0;
-   signal counter_main           : integer                           := 0;
-   --signal blink_adc              : std_logic                         := '0';
-   signal blink_main             : std_logic                         := '0';
-
    ------------------ ADC
    signal adc_slave_waitrequest  : std_logic                         := '0';             -- waitrequest
    signal adc_slave_readdata     : std_logic_vector (31 downto 0)    := (others => '0'); -- readdata
@@ -68,17 +63,17 @@ architecture arch of ADC is
       );
    end component;
 
-   type STATE is (INIT_START, INIT_END, IDLE, WRITE_ADDRESS_0, READ_DATA_0, AVALON_CLEAR, WRITE_ADDRESS_1, READ_DATA_1);
-   --type SEND  is (INIT, WAIT_VALID);
+   type STATE is (INIT_START, INIT_END, IDLE, READ_DATA_0, AVALON_CLEAR, READ_DATA_1);
 
    constant ADDRESS_ADC_0        : std_logic_vector (2 downto 0) := "000";
    constant ADDRESS_ADC_1        : std_logic_vector (2 downto 0) := "001";
    constant ADDRESS_AUTO_UPDATE  : std_logic_vector (2 downto 0) := "001";
    constant ADDRESS_UPDATE       : std_logic_vector (2 downto 0) := "000";   
 
-   signal data_out_0_reg, data_out_1_reg     : std_logic_vector (31 downto 0) := (others => '0');
+   signal memory                             : std_logic_vector (31 downto 0) := (others => '0');
+   signal data_out_0_reg, data_out_1_reg     : std_logic_vector (11 downto 0) := (others => '0');
    signal state_read_reg, state_read_next    : STATE                          := INIT_START;
-   --signal state_send_reg, state_send_next    : SEND                           := INIT;
+
    signal data_reg_0_valid, old_reg_0_valid  : std_logic                      := '0';
    signal data_reg_1_valid, old_reg_1_valid  : std_logic                      := '0';
    signal waitrequest_new, waitrequest_old   : std_logic                      := '0';
@@ -109,35 +104,8 @@ begin
       locked => locked_sig
    );
 
-   ------- STATE SEND Logic -------
-   -- process(state_send_reg, data_reg_valid)
-   -- begin
-   --    case state_send_reg is
-   --       when INIT =>
-   --          -- data_valid_o <= '0';
-   --          if (data_reg_valid = '0') then
-   --             state_send_next <= WAIT_VALID;
-   --          else
-   --             state_read_next <= INIT;
-   --          end if;
-
-   --       when WAIT_VALID =>
-   --          if (data_reg_valid = '1') then
-   --             data_adc_0_o <= data_out_0_reg(11 downto 0);
-   --             data_adc_1_o <= data_out_1_reg(11 downto 0);
-   --             data_valid_o <= '1';
-   --             state_send_next <= INIT;
-   --          else 
-   --             data_adc_0_o <= data_adc_0_o;
-   --             data_adc_1_o <= data_adc_0_o;
-   --             data_valid_o <= '1';
-   --             state_send_next <= INIT;
-   --          end if;
-   --    end case;
-   -- end process;
-
    ------- STATE READ Logic -------
-   process(state_read_reg, waitrequest_old, waitrequest_new)
+   process(all)
    begin
       state_read_next      <= state_read_reg;
    
@@ -147,8 +115,7 @@ begin
       data_reg_0_valid     <= '0';
       data_reg_1_valid     <= '0';
       
-      data_out_0_reg       <= (others => '0');
-      data_out_1_reg       <= (others => '0');
+      memory               <= (others => '0');
       adc_slave_writedata  <= (others => '0');
 
       case state_read_reg is
@@ -172,78 +139,49 @@ begin
             end if;
 
          when IDLE =>
-            adc_slave_read <= '0';
-            data_reg_0_valid <= '0';
-            data_reg_1_valid <= '0';
-            state_read_next <= WRITE_ADDRESS_0;
+            adc_slave_address <= (others => '0');
+            adc_slave_read    <= '0';
+            data_reg_0_valid  <= '0';
+            data_reg_1_valid  <= '0';
+            memory            <= (others => '0');
 
-         when WRITE_ADDRESS_0 =>
-            adc_slave_read <= '1';
-            adc_slave_address <= ADDRESS_ADC_0;
-            -- state_read_next <= READ_DATA_0;
-
-            If(waitrequest_new = '0') then
-               adc_slave_address <= (others => '0');
-               data_out_0_reg <= adc_slave_readdata;
-               data_reg_0_valid <= '1';
-               state_read_next <= AVALON_CLEAR;
-            else
-               data_out_0_reg <= adc_slave_readdata;
-               data_reg_0_valid <= '0';
-               adc_slave_address <= ADDRESS_ADC_0;
-               state_read_next <= WRITE_ADDRESS_0;
-            end if;
+            state_read_next   <= READ_DATA_0;
 
          when READ_DATA_0 =>
-         --    If(waitrequest_new = '0') then
-         --       adc_slave_address <= (others => '0');
-         --       data_out_0_reg <= adc_slave_readdata;
-         --       data_reg_0_valid <= '1';
-         --       state_read_next <= AVALON_CLEAR;
-         --    else
-         --       data_out_0_reg <= adc_slave_readdata;
-         --       data_reg_0_valid <= '0';
-         --       adc_slave_address <= ADDRESS_ADC_0;
-         --       state_read_next <= READ_DATA_0;
-         --    end if;
+            adc_slave_read    <= '1';
+            adc_slave_address <= ADDRESS_ADC_0;
+
+            If(waitrequest_new = '0') then
+               memory               <= adc_slave_readdata;
+               data_reg_0_valid     <= '1';
+               state_read_next      <= AVALON_CLEAR;
+
+            else
+               data_reg_0_valid     <= '0';
+               state_read_next      <= READ_DATA_0;
+               memory            <= (others => '0');
+            end if;
 
          when AVALON_CLEAR =>
-            adc_slave_read <= '0';
-            state_read_next <= WRITE_ADDRESS_1;
+            adc_slave_read    <= '0';
+            state_read_next   <= READ_DATA_1;
             adc_slave_address <= (others => '0');
+            memory            <= (others => '0');
 
-         when WRITE_ADDRESS_1 =>
+         when READ_DATA_1 =>
             adc_slave_read <= '1';
             adc_slave_address <= ADDRESS_ADC_1;
-
+            
             if (waitrequest_new = '0') then
-               adc_slave_address <= (others => '0');
-               data_out_1_reg <= adc_slave_readdata;
+               memory <= adc_slave_readdata;
                data_reg_1_valid <= '1';
                state_read_next <= IDLE;
 
-            -- else
-            --    adc_slave_address <= (others => '0');
-            --    data_out_1_reg <= adc_slave_readdata;
-            --    data_reg_1_valid <= '0';
-            --    state_read_next <= WRITE_ADDRESS_1;
-
+            else 
+               data_reg_1_valid <= '0';
+               state_read_next <= READ_DATA_1;
+               memory            <= (others => '0');
             end if;
-
-         when READ_DATA_1 =>
-         --    if (waitrequest_new = '0') then
-         --       adc_slave_address <= (others => '0');
-         --       data_out_1_reg <= adc_slave_readdata;
-         --       data_reg_1_valid <= '1';
-         --       state_read_next <= IDLE;
-
-         --    else
-         --       adc_slave_address <= (others => '0');
-         --       data_out_1_reg <= adc_slave_readdata;
-         --       data_reg_1_valid <= '0';
-         --       state_read_next <= READ_DATA_1;
-
-         --    end if;
       end case;
    end process;
 
@@ -266,23 +204,21 @@ begin
    begin
       if (reset = '0') then
          -- state_send_reg <= INIT;
-         blink_main <= '0';
-         counter_main <= 0;
-         old_reg_0_valid <= '0';
-         old_reg_1_valid <= '0';
-         data_adc_0_o <= (others => '0');
-         data_adc_1_o <= (others => '0');
-         data_valid_o <= '0';
+         old_reg_0_valid   <= '0';
+         old_reg_1_valid   <= '0';
+
+         data_out_0_reg    <= (others => '0');
+         -- data_out_1_reg    <= (others => '0');
+         data_adc_0_o      <= (others => '0');
+         data_adc_1_o      <= (others => '0');
+         data_valid_o      <= '0';
 
       elsif rising_edge(clk_out) then
-         --state_send_reg <= state_send_next;
-
          if ((not old_reg_0_valid) and (data_reg_0_valid)) then
             old_reg_0_valid <= data_reg_0_valid;
             old_reg_1_valid <= data_reg_1_valid;
             
-            data_adc_0_o <= data_out_0_reg(11 downto 0);
-            data_adc_1_o <= data_adc_1_o;
+            data_out_0_reg <= memory(11 downto 0);
             
             LED0 <= data_out_0_reg(4);
             LED1 <= data_out_0_reg(5);
@@ -293,20 +229,12 @@ begin
             LED6 <= data_out_0_reg(10);
             LED7 <= data_out_0_reg(11);
 
-            data_valid_o <= '0';
-
-            counter_main <= counter_main + 1;
-            if (counter_main = 10000000) then
-               blink_main <= not blink_main;
-               counter_main <= 0;
-            end if;
-
          elsif ((not old_reg_1_valid) and (data_reg_1_valid)) then
             old_reg_0_valid <= data_reg_0_valid;
             old_reg_1_valid <= data_reg_1_valid;
             
-            data_adc_0_o <= data_adc_0_o;
-            data_adc_1_o <= data_out_1_reg(11 downto 0);
+            data_adc_0_o <= data_out_0_reg;
+            data_adc_1_o <= memory(11 downto 0);
             
             data_valid_o <= '1';
 
@@ -322,8 +250,4 @@ begin
          end if;
       end if;
    end process;
-
-   ---------- TB on LEDS -------------
-   LED8 <= blink_main;
-
 end architecture arch;
